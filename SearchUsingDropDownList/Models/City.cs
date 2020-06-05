423bed28ea104be6db196f9786168964f9782f16
Omar Mekawy <omar269991@eng.zu.edu.eg>
2020-06-05 14:19:01 +0200

    Add project files.

---
+++ SearchUsingDropDownList/Models/City.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchUsingDropDownList.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string CityNameAR { get; set; }
+        public string CityNameEn { get; set; }
+        public int  GovernId { get; set; }
+    }
+}
---
+++ SearchUsingDropDownList/Models/Governorate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchUsingDropDownList.Models
+{
+    public class Governorate
+    {
+        public int Id { get; set; }
+        public string GovNameAR { get; set; }
+        public string GovNameEN { get; set; }
+        public int CityId { get; set; }
+
+        public IEnumerable<City> Cities { get; set; }
+    }
+}
